8f519f666250ebc0b39bc853e4f67ef007f6d481
S H Mohanjith <moha@mohanjith.net>
2012-12-27 20:22:56 +0200

    i

--- lean-media.php
+++ lean-media.php
@@ -8,9 +8,23 @@ Author: S H Mohanjith (Code Master Oy)
 Author URI: http://codemaster.fi/
 */
 
-add_filter('wp_generate_attachment_metadata', 'delete_fullsize_image');
+add_action('init', 'lean_media_init');
+add_action('admin_init', 'lean_media_admin_init');
 
-function delete_fullsize_image($metadata) {
+function lean_media_init() {
+    load_plugin_textdomain('lean-media', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    
+    if (get_option('lean_media_settings-delete_large_files', 1) == 1) {
+        add_filter('wp_generate_attachment_metadata', 'lean_media_delete_fullsize_image');
+    }
+}
+
+function lean_media_admin_init() {
+    add_settings_field('lean_media_settings-delete_large_files', __('Delete Large Images', 'lean-media'), 'lean_media_setting_delete_large_files_callback', 'media');
+    register_setting('media', 'lean_media_settings-delete_large_files', 'intval');
+}
+
+function lean_media_delete_fullsize_image($metadata) {
     
     $upload_dir = wp_upload_dir();
     $full_image_path = trailingslashit( $upload_dir['basedir'] ) . $metadata['file'];
@@ -32,10 +46,19 @@ function delete_fullsize_image($metadata) {
     
     $full_large_thumb_path = trailingslashit( $upload_dir['path'] ) . $largest_thumbnail;
     
-    if ($metadata['width'] > $width || $metadata['height'] > $height) {
+    if ($metadata['width'] > get_option('large_size_w') || $metadata['height'] > get_option('large_size_h')) {
         $deleted = @unlink( $full_image_path );
         $copied = @copy($full_large_thumb_path, $full_image_path);
+        
+        $metadata['width'] = $width;
+        $metadata['height'] = $height;
     }
     
     return $metadata;
+}
+
+function lean_media_setting_delete_large_files_callback() {
+    ?>
+    <input id="lean_media_settings-delete_large_files" name="lean_media_settings-delete_large_files" type="checkbox" value="1" <?php echo (get_option('lean_media_settings-delete_large_files', 1) == 1)?'checked="checked"':''; ?> />
+    <?php
 }
